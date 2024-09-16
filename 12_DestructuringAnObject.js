2bbba3c3c5bf74237e134c736f7c57ad8b628633
Adarsh-Chaubey03 <adarsh03chaubey@gmail.com>
2024-09-16 19:18:11 +0530

    Create 12_DestructuringAnObject.js

---
+++ 12_DestructuringAnObject.js
@@ -0,0 +1,37 @@
+
+const course = {
+    coursename : "JS in hindi",
+    courseprice: " 999 ",
+Instructor: "Adarsh"
+}
+
+const{Instructor} = course
+
+console.log(Instructor);  //Adarsh
+
+/*
+const navbar = ({company}) => {
+
+}
+
+navbar(company = "Adarsh")
+*/
+
+/* ---------------- API  --------------------
+
+{
+  "name": "Adarsh",
+  "coursename": "js in hindi",
+  "price": "0"
+}
+
+*/
+
+
+[
+    {},
+    {},
+    {},
+    {},
+    {},
+]
