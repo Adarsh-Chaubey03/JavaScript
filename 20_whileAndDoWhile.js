fcc5cc6319f83de7d419b1dd51a32aefc62ad56e
Adarsh-Chaubey03 <adarsh03chaubey@gmail.com>
2024-09-19 17:10:33 +0530

    Update 20_whileAndDoWhile.js

--- 20_whileAndDoWhile.js
+++ 20_whileAndDoWhile.js
@@ -1,4 +1,15 @@
+// while loop
+
 let i = 0;
 while (i <= 10) {
     console.log(i)
+    i += 3;
 }
+
+// do while
+
+let score = 11
+do {
+    console.log(score);      // 11
+    score++;                     
+} while (score <= 10);
