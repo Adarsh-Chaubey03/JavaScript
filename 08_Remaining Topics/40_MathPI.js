2a8b59f2ff3be971566ca3839656563e7b4cf820
Adarsh-Chaubey03 <adarsh03chaubey@gmail.com>
2025-06-02 23:15:22 +0530

    JS Closure

--- 08_Remaining Topics/40_MathPI.js
+++ 08_Remaining Topics/40_MathPI.js
@@ -1,5 +1,7 @@
 console.log(Math.PI); //3.141592653589793
 
-const descriptor = Object.getOwnPropertyDescriptor(Math,"PI")
+const descriptor = Object.getOwnPropertyDescriptor(Math, "PI")
 
 console.log(descriptor)
+
+// {value: 3.141592653589793, writable: false, enumerable: false, configurable: false}
